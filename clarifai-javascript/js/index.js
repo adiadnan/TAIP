bda4b684d7a6ed3d31234f05115fb910bb63318f
RaresMunteanu <dan.rares.munteanu@gmail.com>
2016-11-15 12:55:21 +0200

    Update index.js
    Cleaned the code, no major features added

--- clarifai-javascript/js/index.js
+++ clarifai-javascript/js/index.js
@@ -3,14 +3,10 @@
  	// function to initialize the keys
 	function initialize (clientId, clientSecret) {
 		// getting the credential through calling getKeys()
-		// which is available in Global scope because of keys.js
-		//var keys = getKeys() || {}
 
 		this.clientId = clientId;
 		this.clientSecret=clientSecret;
 
-		//var clientId = keys["CLARIFAI_CLIENT_ID"]
-		//var clientSecret = keys["CLARIFAI_CLIENT_SECRET"]
 }
 
 function initialize(){}
@@ -18,18 +14,12 @@ function initialize(){}
 
 function check(clientId, clientSecret)
 {
-		//if (!clientId || !clientSecret) {
-			//console.log("Enter your Clarifai's Client ID and Client Secret in order to successfully run this demo. Go to developer.clarifai.com, sign up and create your application if you haven't already. You'll have to edit keys.js file to enter your credentials")
-		//}
-		//else {
-			// TODO
+		
 			// Initialize the Clarifai api here			
 			Clarifai.initialize({
 				clientId: clientId,
 				clientSecret: clientSecret
 			})
-		//}
-
 
 	}
 
@@ -43,11 +33,6 @@ var obtainKeys= (function() {
 
 }})();
 
-
-
-
-//var keys = getKeys() || {}
-
 var init = new initialize();
 initialize.prototype.clientId=obtainKeys.Keys()["CLARIFAI_CLIENT_ID"];
 initialize.prototype.clientSecret=obtainKeys.Keys()["CLARIFAI_CLIENT_SECRET"];
@@ -58,14 +43,13 @@ var printer = (function () {
   function create () {
 
     function print() {
-      // underlying printer mechanics
-      console.log(init.clientSecret, init.clientId)
+     
+	 console.log(init.clientSecret, init.clientId)
     }
 
 
     return {
-      // public + private states and behaviors
-      print: print
+         print: print
     };
   }
 
@@ -104,21 +88,12 @@ var Printer = printer.getInstance();
 	var tags = $(".tags");
 
 	submitButton.on("click", function (event) {
-		// getting the input from the image
 		var url = imageInput.val()
 		tagsContainer.hide()
 
-		// You can ignore this part
-		// Set's the url of the image preview
 		image.attr("src", url)
 
-
-		/*
-		 * TODO
-		 * request Clarifai tag for the url by using Clarifai.getTagsByUrl
-		 */
-
-		 Clarifai.getTagsByUrl(url,function(error,response) {
+		Clarifai.getTagsByUrl(url,function(error,response) {
 
 		 	if(error)
 		 	{
@@ -130,52 +105,28 @@ var Printer = printer.getInstance();
 		 	}
 		 })
 
-		/*
-		 * TODO
-		 * request colors for the image by using Clarifai function to get *colors by url. 
-		 */
-
 	})
 
-	/*
-	 * displayTag
-	 * functionality to display the tag with classes and probabilities
-	 * contains some functionality regarding the response manipulation
-	 */
+	
 	function displayTag (response) {
 		console.log("Clarifai Response!")
 		console.log(response)
 
-		/* 
-		 * an array of resultant images is received
-		 * in this case we only request one 
-		 * so we just need to get the first one
-		 */
 		var image 	= 	response.results[0]
-
-		// Image has a further tag that contains classes, concept_ids and 
-		// probabilities for each concept
 		var tag 	= 	image.result.tag
-
 		var conceptsLength = tag.classes.length
-
-		// Looping through all the classes in the tag using map
-		// to get the html for each concept
 		var concepts = tag.classes.map(function (value, index, array) {
 			var prob = Number.parseFloat(tag.probs[index]);
 			var probPercentage = (prob * 100).toString() + "%";
-
-			// Assigning color for the progressBar using `assignColor`
+			
 			var progressBarColor = assignColor(prob, index, conceptsLength)
 
-			// Generating progress bar using values and a template
 			var progressBar = `<div class="progress">
 						<div class="progress-bar progress-bar-${progressBarColor}" role="progressbar" aria-valuenow="${prob}" aria-valuemin="0" aria-valuemax="100" style="width: ${probPercentage};">
 							${prob}
 						</div>
 					</div>`
 
-			// adding class and concept information
 			return `<div class="row">
 						<div class="col-sm-12">
 							<h3>${value}</h3>
@@ -185,19 +136,13 @@ var Printer = printer.getInstance();
 					</div>`
 		})
 
-		// joining all the stuff generated and throwing the html into .tags
 		tags.html(concepts.join(""))
 
-		// displaying the hidden container
+
 		tagsContainer.show()
 
 	}
 
-
-	/*
-	 * assignColor
-	 * returns the color for progress bar based on the probability
-	 */
 	function assignColor (prob, index, length) {
 		if (prob > 0.9) {
 			return "success"
@@ -210,7 +155,6 @@ var Printer = printer.getInstance();
 		}
 	}
 
-	// In case of error displays an error in the Clarifai 
 	function displayError (error) {
 
 		// Preparing the error message
